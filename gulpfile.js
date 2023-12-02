577407e9182a17abd8e308ec24d2c6817a6a1d38
DESKTOP-I78HJ1K\Yulianna <https://github.com/AndrusykXD/FX-8320>
2023-12-02 21:47:41 +0200

    Add some tasks and basic marking

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,12 @@
-const {series,src,dest,parallel,watch} = require("gulp");
-
-// Створюємо текстовий таск
-
-// Запуск тасків за змовчуванням
-
-var sass = require('gulp-sass')(require('sass')), // конвертує SASS в CSS
-    cssnano = require("gulp-cssnano"), // мінімізація CSS
-    autoprefixer = require("gulp-autoprefixer"),
-    imagemin = require("gulp-imagemin"),
-    concat = require("gulp-concat"),
-    uglify = require("gulp-uglify"),
-    rename = require("gulp-rename");
+const {series, src, dest, parallel, watch} = require('gulp');
+const sass = require ('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const cssnano = require('gulp-cssnano');
 
 const html_task = () => src('app/*.html')
     .pipe(dest('dist'));
-
 const css_task = () => src('app/css/*.css')
     .pipe(dest('dist/css'));
 const scss_task = () => src('app/scss/*.scss')
@@ -28,19 +20,11 @@ const scripts_task = () =>src('app/js/*.js')
     .pipe(uglify())
     .pipe(rename({suffix:'.min'}))
     .pipe(dest('dist/js'))
-const img_task = () => src('app/img/*.+(jpg|png|jpeg|gif)')
-    .pipe(imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
-        interlaced: true
-    }))
-    .pipe(dest('dist/img'))
 const watch_task = () => {
     watch('app/*.html', parallel(html_task));
     watch('app/scss/*.scss', parallel(scss_task));
     watch('app/js/*.js', parallel(scripts_task));
-    watch('app/img/*.+(jpg|png|jpeg|gif)', parallel(img_task));
+
 }
 
-exports.default = series(html_task,scss_task,scripts_task,css_task,img_task, watch_task);
-kjhgfr
+exports.default = series(html_task,scss_task,scripts_task,css_task, watch_task);
